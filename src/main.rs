c54550d668ad19d16abc803b5b8f4f3b9dd0d999
ipigtw <diegogamingtw@gmail.com>
2024-07-13 20:14:50 +0800

    added multi-threading(faster)

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,30 @@ use std::{
 };
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() {
     let client = reqwest::Client::new();
     let mut tokenlist = String::new();
     print!("Tokens list: ");
     io::stdout().flush().unwrap();
-    io::stdin()
-        .read_line(&mut tokenlist)
-        .expect("Failed to read token input");
+    io::stdin().read_line(&mut tokenlist).unwrap();
     let tokens = fs::read_to_string(tokenlist.trim()).unwrap();
-    for token in tokens.split("\n") {
-        let token = token.trim();
-        let resp = client
-            .get("https://discord.com/api/v9/users/@me")
-            .header("Authorization", token)
-            .send()
-            .await?;
-        if resp.status().is_success() {
-            println!("{} is valid", token);
-        } else {
-            println!("{} is invalid", token);
+    loop {
+        for token in tokens.split("\n") {
+            let token = token.trim();
+            tokio::spawn(check_token(client.clone(), String::from(token)));
         }
     }
-    Ok(())
+}
+async fn check_token(client: reqwest::Client, token: String) {
+    let resp = client
+        .get("https://discord.com/api/v9/users/@me")
+        .header("Authorization", &token)
+        .send()
+        .await
+        .unwrap();
+    if resp.status().is_success() {
+        println!("{} is valid", token);
+    } else {
+        println!("{} is invalid", token)
+    }
 }
